57be0e16105749c264fd4d4059b75efc69f760ed
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-08 14:57:54 +0200

    read the budget

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from "react";
 
 const Form = () => {
-    return ( 
-        <Fragment>
-            <h2>Write your budget</h2>
-            <form>
-                <input
-                    type='number'
-                    className='u-full-width'
-                    placeholder='Add your budget'
-                >
-                </input>
-                <input
-                    type='submit'
-                    className='button-primary u-full-width'
-                    placeholder='Define budget'
-                >
-                </input>
-            </form>
-        </Fragment>
-     );
-}
- 
+
+  const [quantity, saveQuantity] = useState(0);
+
+  const defineBudget = (e) => {
+    saveQuantity( parseInt(e.target.value));
+  }
+
+  const addBudget = (e) => {
+    e.preventDefault();
+    // validate
+    // si pasa la validation
+  }
+
+  return (
+    <Fragment>
+      <h2>Write your budget</h2>
+      <form
+        onSubmit='addBudget'
+      >
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Add your budget"
+          onChange={defineBudget}
+        ></input>
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          placeholder="Define budget"
+        ></input>
+      </form>
+    </Fragment>
+  );
+};
+
 export default Form;
