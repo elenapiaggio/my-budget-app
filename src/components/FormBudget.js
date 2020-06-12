4734d93d719925d4a862c08b650ab8e9af7da81b
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-13 01:28:54 +0200

    ADD state to expensive

--- src/components/FormBudget.js
+++ src/components/FormBudget.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FormBudget = () => {
+  const [expensive, saveExpensive] = useState("");
+  const [quantity, saveQuantity] = useState(0);
+
+  const addExpensive = (e) => {
+    e.preventDefault();
+
+    // validate
+    // build the expensive
+    // set expensive to principal component
+    // reset form
+  }
   return (
-    <form>
+    <form
+      onSubmit = {addExpensive}
+    >
       <h2>Add your expenses</h2>
       <div className="campo">
         <label>Expense</label>
@@ -10,6 +23,8 @@ const FormBudget = () => {
           type="text"
           className="u-full-width"
           placeholder="TMB, Supermarket, Coffes ..."
+          value={expensive}
+          onChange={(e) => saveExpensive(e.target.value)}
         />
       </div>
       <div className="campo">
@@ -18,6 +33,8 @@ const FormBudget = () => {
           type="number"
           className="u-full-width"
           placeholder="100, 200, 1000 ..."
+          value={quantity}
+          onChange={(e) => saveQuantity(parseInt(e.target.value))}
         />
       </div>
       <input type="submit" className="button-primary u-full-width" />
